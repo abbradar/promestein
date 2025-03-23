6142cb1872552f7ea6c1a92658c13af8a1556b16
Nikolay Amiantov <ab@fmap.me>
2025-03-23 14:28:02 +0700

    Update gamescope-screenshot

--- server/scripts/make-gamescope-screenshot.sh
+++ server/scripts/make-gamescope-screenshot.sh
@@ -11,5 +11,18 @@ cleanup() {
 }
 trap cleanup EXIT
 
-gamescopectl screenshot "$dir/image.png"
-curl -F "image=@$dir/image.png" "$host/pages/new/image"
+image="$dir/image.png"
+gamescopectl screenshot "$image"
+# It's async, gamescope is running from root and we don't even have inotifywait.
+# This is the best I could do.
+for i in $(seq 1 100); do
+  size=$(stat --printf="%s" "$image" 2>/dev/null || true)
+  # Treat the file as finished when the size is non-zero and stopped growing.
+  if [ -n "$size" ] && [ "$size" != "0" ] && [ "$size" = "$old_size" ]; then
+    break
+  else
+    old_size="$size"
+    sleep 0.01
+  fi
+done
+curl -F "image=@$image" "$host/pages/new/image"
